08755876adc715c64dadccfe2be19c78daed9f53
eyobN <eyobnigusework@gmail.com>
2025-11-25 14:50:43 +0300

    crud added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -24,3 +24,28 @@ export const fetchTeam = async (id) => {
     const { data } = await axiosInstance.get(`/api/team/${id}`);
     return data;
 }
+export const createProject = async (project) => {
+    const { data } = await axiosInstance.post(`/api/projects`, project);
+    return data;
+}
+export const updateProject = async (id, projectData) => {
+    const { data } = await axiosInstance.put(`/api/projects/${id}`, projectData);
+    return data;
+}
+export const deleteProject = async (id) => {
+    const { data } = await axiosInstance.delete(`/api/projects/${id}`);
+    return data;
+}
+export const createTeam = async (team) => {
+    const { data } = await axiosInstance.post(`/api/team`, team);
+    return data;
+}
+export const updateTeam = async (id, teamData) => {
+    const { data } = await axiosInstance.put(`/api/team/${id}`, teamData);
+    return data;
+}
+export const deleteTeam = async (id) => {
+    const { data } = await axiosInstance.delete(`/api/team/${id}`);
+    return data;
+}
+
